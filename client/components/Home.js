21d1b922fdd93744bc33430e26152890afc8ea18
Gustavo Allen <gustavoallen92@gmail.com>
2021-11-05 17:27:57 +0000

    fixed homepage component

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Homepage = (props) => (
-    <div className="main">
-        Hello World!!!!!
-    </div>
-)
+class Homepage extends React.Component
+{
+    render()
+    {
+        return (
+            <div className="main">
+                Hello World!!!!!
+            </div>
+
+        )
+    }
+}
 
 export default Homepage
